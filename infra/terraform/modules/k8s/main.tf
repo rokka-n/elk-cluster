2225b7c3f8287e0c5ea6f962eb0e432b0e5e83ea
Roman Naumenko <roman@naumenko.ca>
2016-10-09 17:41:01 -0700

    Expose port 80 on k8s master

--- infra/terraform/modules/k8s/main.tf
+++ infra/terraform/modules/k8s/main.tf
@@ -34,6 +34,13 @@ resource "aws_security_group" "allow_ssh" {
       cidr_blocks = ["${var.vpc_cidr}"]
   }
 
+  // expose nginx
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
       from_port = 0
